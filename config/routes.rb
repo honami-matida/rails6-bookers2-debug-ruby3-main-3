24f674002a81038c7a3b67f3c98a2b1c118725b5
honami-matida <yuzuyuzu202405@gmail.com>
2024-06-09 23:11:01 +0000

    [Finish]応用課題9d

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,7 @@ Rails.application.routes.draw do
     get "send/mail" => "groups#send_mail"
   end
 
+  get 'tagsearches/search', to: 'tagsearches#search'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
